3a3a2710e4bab2a9fcf32d552ff7ff1b67f7fe0f
Abir <abirmahmud221@gmail.com>
2024-01-17 19:37:48 +0600

    steps section completed

--- Prerok-Global-Client/src/Components/Homepage/Steps/Steps.jsx
+++ Prerok-Global-Client/src/Components/Homepage/Steps/Steps.jsx
@@ -1,18 +1,46 @@
-import deliveryTruck from "../../../../public/images/deliveryTruck.png"
+import deliveryTruck from "../../../../public/images/deliveryTruck.png";
 
 const Steps = () => {
-    return (
-        <div className="h-72 border bg-[url('../../../../public/images/city.jpg')] flex justify-between items-center ">
-            
-                <div>
-                    <h1>ok</h1>
-                </div>
-                <div>
-                    <img src={deliveryTruck} alt="" className="mt-11"/>
-                </div>
-            
+  return (
+    <div className="h-72 border bg-[url('../../../../public/images/city.jpg')] bg-opacity-25 flex justify-between items-center ">
+      <div className="flex justify-around text-white px-12">
+        <div className="flex justify-between items-center">
+          <h1 className="text-7xl font-semibold">1.</h1>
+          <div className=" ml-3">
+            <h3 className="text-2xl font-semibold">Order</h3>
+            <p className="w-3/4 mt-2 text-[#98968c] text-sm">
+              Duis autem vel eum iriur hendrerit in vulputat
+            </p>
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex justify-around text-white ">
+        <div className="flex justify-between items-center">
+          <h1 className="text-7xl font-semibold">2.</h1>
+          <div className=" ml-3">
+            <h3 className="text-2xl font-semibold">Wait</h3>
+            <p className="w-3/4 mt-2 text-[#98968c] text-sm">
+              Duis autem vel eum iriur hendrerit in vulputat
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-around text-white">
+        <div className="flex justify-between items-center">
+          <h1 className="text-7xl font-semibold">3.</h1>
+          <div className=" ml-3">
+            <h3 className="text-2xl font-semibold">Deliver</h3>
+            <p className="w-3/4 mt-2 text-[#98968c] text-sm">
+              Duis autem vel eum iriur hendrerit in vulputat
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={deliveryTruck} alt="" className="mt-16" />
+      </div>
+    </div>
+  );
 };
 
 export default Steps;
