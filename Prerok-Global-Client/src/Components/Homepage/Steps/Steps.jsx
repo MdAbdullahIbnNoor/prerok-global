0f32dbe198ee263274ed84620c0e900fa3a8b289
Abir <abirmahmud221@gmail.com>
2024-01-18 12:26:08 +0600

    steps section mobile responsieveness updated

--- Prerok-Global-Client/src/Components/Homepage/Steps/Steps.jsx
+++ Prerok-Global-Client/src/Components/Homepage/Steps/Steps.jsx
@@ -3,13 +3,13 @@ import deliveryTruck from "../../../../public/images/deliveryTruck.png";
 const Steps = () => {
   return (
     <div className="h-72 border bg-[url('../../../../public/images/city.jpg')]">
-      <div className="relative bg-black/70 flex justify-between items-center ">
-      <div className="flex justify-around text-white px-12">
+      <div className="relative bg-black/70 flex flex-col md:flex-row justify-between items-center ">
+      <div className="flex flex-row justify-around text-white px-12">
         <div className="flex justify-between items-center">
           <h1 className="text-7xl font-semibold">1.</h1>
           <div className=" ml-3">
             <h3 className="text-2xl font-semibold">Order</h3>
-            <p className="w-3/4 mt-2 text-[#98968c] text-sm">
+            <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
               Duis autem vel eum iriur hendrerit in vulputat
             </p>
           </div>
@@ -20,7 +20,7 @@ const Steps = () => {
           <h1 className="text-7xl font-semibold">2.</h1>
           <div className=" ml-3">
             <h3 className="text-2xl font-semibold">Wait</h3>
-            <p className="w-3/4 mt-2 text-[#98968c] text-sm">
+            <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
               Duis autem vel eum iriur hendrerit in vulputat
             </p>
           </div>
@@ -31,14 +31,14 @@ const Steps = () => {
           <h1 className="text-7xl font-semibold">3.</h1>
           <div className=" ml-3">
             <h3 className="text-2xl font-semibold">Deliver</h3>
-            <p className="w-3/4 mt-2 text-[#98968c] text-sm">
+            <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
               Duis autem vel eum iriur hendrerit in vulputat
             </p>
           </div>
         </div>
       </div>
       <div>
-        <img src={deliveryTruck} alt="" className="mt-16" />
+        <img src={deliveryTruck} alt="" className="md:mt-16 hidden md:block" />
       </div>
       </div>
     </div>
