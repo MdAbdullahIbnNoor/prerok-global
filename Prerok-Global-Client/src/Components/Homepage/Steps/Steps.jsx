9bf4fcc7f3d0affc0981dcc3440d50bd1131c433
Abir <abirmahmud221@gmail.com>
2024-01-18 12:41:28 +0600

    delivery section mobile respnsiveness completed

--- Prerok-Global-Client/src/Components/Homepage/Steps/Steps.jsx
+++ Prerok-Global-Client/src/Components/Homepage/Steps/Steps.jsx
@@ -2,44 +2,44 @@ import deliveryTruck from "../../../../public/images/deliveryTruck.png";
 
 const Steps = () => {
   return (
-    <div className="h-72 border bg-[url('../../../../public/images/city.jpg')]">
-      <div className="relative bg-black/70 flex flex-col md:flex-row justify-between items-center ">
-      <div className="flex flex-row justify-around text-white px-12">
-        <div className="flex justify-between items-center">
-          <h1 className="text-7xl font-semibold">1.</h1>
-          <div className=" ml-3">
-            <h3 className="text-2xl font-semibold">Order</h3>
-            <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
-              Duis autem vel eum iriur hendrerit in vulputat
-            </p>
+    <div className="h-72 bg-[url('../../../../public/images/city.jpg')]">
+      <div className="relative bg-black/70 flex  justify-between items-center ">
+        <div className="flex justify-around text-white px-12">
+          <div className="flex justify-between items-center">
+            <h1 className="text-7xl font-semibold">1.</h1>
+            <div className=" ml-3">
+              <h3 className="text-2xl font-semibold">Order</h3>
+              <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
+                Duis autem vel eum iriur hendrerit in vulputat
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-around text-white ">
-        <div className="flex justify-between items-center">
-          <h1 className="text-7xl font-semibold">2.</h1>
-          <div className=" ml-3">
-            <h3 className="text-2xl font-semibold">Wait</h3>
-            <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
-              Duis autem vel eum iriur hendrerit in vulputat
-            </p>
+        <div className="flex justify-around text-white ">
+          <div className="flex justify-between items-center">
+            <h1 className="text-7xl font-semibold">2.</h1>
+            <div className=" ml-3">
+              <h3 className="text-2xl font-semibold">Wait</h3>
+              <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
+                Duis autem vel eum iriur hendrerit in vulputat
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-around text-white">
-        <div className="flex justify-between items-center">
-          <h1 className="text-7xl font-semibold">3.</h1>
-          <div className=" ml-3">
-            <h3 className="text-2xl font-semibold">Deliver</h3>
-            <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
-              Duis autem vel eum iriur hendrerit in vulputat
-            </p>
+        <div className="flex justify-around text-white">
+          <div className="flex justify-between items-center">
+            <h1 className="text-7xl font-semibold">3.</h1>
+            <div className=" ml-3">
+              <h3 className="text-2xl font-semibold">Deliver</h3>
+              <p className="md:w-3/4 mt-2 text-[#98968c] text-sm">
+                Duis autem vel eum iriur hendrerit in vulputat
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <img src={deliveryTruck} alt="" className="md:mt-16 hidden md:block" />
-      </div>
+        <div>
+          <img src={deliveryTruck} alt="" className="md:mt-16" />
+        </div>
       </div>
     </div>
   );
