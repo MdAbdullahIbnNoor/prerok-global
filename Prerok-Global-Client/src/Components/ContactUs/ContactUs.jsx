40ccff1a28683498902dcd64374a8c769775842f
Abir <abirmahmud221@gmail.com>
2024-01-18 22:34:05 +0600

    added contact us component and completed the first part

---
+++ Prerok-Global-Client/src/Components/ContactUs/ContactUs.jsx
@@ -0,0 +1,19 @@
+
+
+const ContactUs = () => {
+    return (
+        <div className="my-10 border border-red-500">
+            <div className="flex justify-between items-center px-20">
+                <div>
+                    <h1 className="text-3xl text-[#222222] font-bold">CONTACT US</h1>
+                    <p className="text-xl text-[#acacac]">Get in touch with us easily</p>
+                </div>
+                <div>
+                    <p className="text-sm text-[#acacac]">Home / Contact</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactUs;
--- Prerok-Global-Client/src/router.jsx
+++ Prerok-Global-Client/src/router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import ContactUs from "./Components/ContactUs/ContactUs";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
   },
+  {
+    path: "/contact",
+    element: <ContactUs></ContactUs>,
+  },
+
 ]);
 
 export default router;
