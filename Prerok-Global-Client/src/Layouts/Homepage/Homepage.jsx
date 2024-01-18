b40713c207e8b2d06096bd4bfded9a8a314c0cd8
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2024-01-18 22:50:40 +0600

    testimonial and pricing section added

--- Prerok-Global-Client/src/Layouts/Homepage/Homepage.jsx
+++ Prerok-Global-Client/src/Layouts/Homepage/Homepage.jsx
@@ -1,5 +1,11 @@
+import Pricing from "../../Components/Homepage/Pricing/Pricing";
+import Testimonial from "../../Components/Homepage/Testimonial/Testimonial";
+
 const Homepage = () => {
-  return <div>Homepage.jsx</div>;
+  return <div>
+    <Testimonial/>
+    <Pricing/>
+  </div>;
 };
 
 export default Homepage;
