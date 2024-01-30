c78b8151fae159d8ad753dc0cdedced1572efdcd
Abir <abirmahmud221@gmail.com>
2024-01-30 22:46:37 +0600

    upper section complete

--- Prerok-Global-Client/src/Components/PricingPage/PricingPage.jsx
+++ Prerok-Global-Client/src/Components/PricingPage/PricingPage.jsx
@@ -2,8 +2,14 @@
 
 const PricingPage = () => {
     return (
-        <div>
-            pricing
+        <div className="w-11/12 mx-auto">
+            <div className="bg-slate-500 text-white flex justify-between px-3 py-3">
+                <h2 className="text-2xl">PrerokGlobal Ship Manager</h2>
+                <div className="flex items-center gap-5 text-sm">
+                    <div>Advance Shipping</div>
+                    <div>Help</div>
+                </div>
+            </div>
         </div>
     );
 };
