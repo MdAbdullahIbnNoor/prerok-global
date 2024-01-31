ef3d786e0f9c87edf6e2f50537ea7c34ad5672aa
Abir <abirmahmud221@gmail.com>
2024-01-31 19:52:05 +0600

    1st part. address information completed

--- Prerok-Global-Client/src/Components/PricingPage/PricingPage.jsx
+++ Prerok-Global-Client/src/Components/PricingPage/PricingPage.jsx
@@ -22,17 +22,101 @@ const PricingPage = () => {
                     <h2 className="text-2xl mt-2 text-amber-500">Enter your (From) address and the recipient&apos;s (To) address.</h2>
                 </div>
                 <div>
-                    <form className="flex items-center gap-10">
+                    <form className="flex  gap-10">
                         <div className="w-1/2">
                             <h4 className="font-semibold text-amber-500 ml-5">From Address</h4>
                             <hr className="ml-5"/>
                             <div>
-
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Your Name:</div>
+                                    <div><input type="text" className="border px-2 " name="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Company:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="(optional)" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Country / Location:</div>
+                                    <div><input type="text" className="border px-2" name="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Address:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="Street Address" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div></div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="APT,Floor, Suite etc." id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Postal Code:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>City:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Phone:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Email:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Tax ID no.</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                
                             </div>
                         </div>
                         <div className="w-1/2">
                             <h4 className="font-semibold text-amber-500">To Address</h4>
                             <hr />
+                            <div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Recipient Name:</div>
+                                    <div><input type="text" className="border px-2" name="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Company:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="(optional)" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Country / Location:</div>
+                                    <div><input type="text" className="border px-2" name="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Address:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="Street Address" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div></div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="APT,Floor, Suite etc." id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Postal Code:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>City:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Phone:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-between  p-3 px-12">
+                                    <div>Email:</div>
+                                    <div><input type="text" className="border px-2" name="" placeholder="" id="" /></div>
+                                </div>
+                                <div className="flex justify-end gap-5 p-3 px-12">
+                                    
+                                    <div><input type="checkbox" name="" id="" /></div>
+                                    <div>This is residential address</div>
+                                </div>
+                                
+                            </div>
                         </div>
                     </form>
                 </div>
