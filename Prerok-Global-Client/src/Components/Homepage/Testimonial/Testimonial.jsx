eb3ec8542f180e4a005d56ebeeb742fd8f41d2ab
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2024-02-02 18:50:28 +0600

    responsive issue solved

--- Prerok-Global-Client/src/Components/Homepage/Testimonial/Testimonial.jsx
+++ Prerok-Global-Client/src/Components/Homepage/Testimonial/Testimonial.jsx
@@ -11,7 +11,7 @@ const Testimonial = () => {
       {/* Static Background Image */}
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")' }}
+        style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1606185540834-d6e7483ee1a4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")' }}
       ></div>
 
       <div className="absolute inset-0 bg-black opacity-60"></div>
