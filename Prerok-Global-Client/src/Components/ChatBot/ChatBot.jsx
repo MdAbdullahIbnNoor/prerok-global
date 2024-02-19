975392da835bd416af7327a377c8c257e26d03c8
Shanta Islam <shantoislam7363@gmail.com>
2024-02-20 02:49:25 +0600

    Meta developer account is created and appId is added

--- Prerok-Global-Client/src/Components/ChatBot/ChatBot.jsx
+++ Prerok-Global-Client/src/Components/ChatBot/ChatBot.jsx
@@ -2,7 +2,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const ChatBot = () => {
     return (
-        <FacebookProvider appId="123456789" chatSupport>
+        <FacebookProvider appId="904560501048331" chatSupport>
             <CustomChat pageId="260428917145639" minimized={false} />
         </FacebookProvider>
     );
