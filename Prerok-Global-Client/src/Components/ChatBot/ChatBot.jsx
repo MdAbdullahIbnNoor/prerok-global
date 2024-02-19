36f6429a86ad0e798ddde32a65ccce006e39fd73
Shanta Islam <shantoislam7363@gmail.com>
2024-02-20 02:38:36 +0600

    Meta fb page created for prerok global chatbot and page id is added

--- Prerok-Global-Client/src/Components/ChatBot/ChatBot.jsx
+++ Prerok-Global-Client/src/Components/ChatBot/ChatBot.jsx
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 const ChatBot = () => {
     return (
         <FacebookProvider appId="123456789" chatSupport>
-            <CustomChat pageId="123456789" minimized={false} />
+            <CustomChat pageId="260428917145639" minimized={false} />
         </FacebookProvider>
     );
 };
