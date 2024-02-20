d624a68be6eaa0d91701599386a946d15e211904
Shanta Islam <shantoislam7363@gmail.com>
2024-02-20 15:28:14 +0600

    Minimization is added so that people can minimize this

--- Prerok-Global-Client/src/Components/ChatBot/ChatBot.jsx
+++ Prerok-Global-Client/src/Components/ChatBot/ChatBot.jsx
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 const ChatBot = () => {
     return (
         <FacebookProvider appId="904560501048331" chatSupport>
-            <CustomChat pageId="260428917145639" minimized={false} />
+            <CustomChat pageId="260428917145639" minimized={true} />
         </FacebookProvider>
     );
 };
