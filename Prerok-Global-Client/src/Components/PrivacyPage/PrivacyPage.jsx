6d7e9cc8875c9e4bae5cdecaf7b62edc43f2295a
Abir <abirmahmud221@gmail.com>
2024-02-23 11:31:42 +0600

    added the banner

--- Prerok-Global-Client/src/Components/PrivacyPage/PrivacyPage.jsx
+++ Prerok-Global-Client/src/Components/PrivacyPage/PrivacyPage.jsx
@@ -1,11 +1,24 @@
-
-
 const PrivacyPage = () => {
-    return (
-        <div>
-            this is privacy
+  return (
+    <div>
+      <div
+        className="hero min-h-max"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1504711331083-9c895941bf81?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-right text-neutral-content">
+          <div className="max-w-md py-8">
+            <h1 className="mb-5 text-3xl font-semibold">PrerokGlobal Privacy Notice</h1>
+           
+           
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PrivacyPage;
