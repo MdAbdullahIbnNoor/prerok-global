c4e83bc8481a2b2dc67ad220d55e9e9fa4521c9e
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-02-25 15:49:13 +0600

    forum apis updated

--- Prerok-Global-Client/src/Layouts/Forum/SinglePost/SinglePost.jsx
+++ Prerok-Global-Client/src/Layouts/Forum/SinglePost/SinglePost.jsx
@@ -12,14 +12,13 @@ const SinglePost = () => {
   console.log(postId);
   useEffect(() => {
     axiosPublic.get(`api/forum/get-forum/${postId}`).then((res) => {
-      console.log(res.data);
       setPostData(res.data);
     });
   }, [postId, setPostData]);
   return (
     <div className="border border-[#f5ab35] w-full lg:w-6/12 mx-auto mt-2 mb-4 pt-2 pb-3 shadow-xl p-4 h-full my-3">
-      <div className="overflow-hidden h-full">
-        <h4 className="text-xl font-semibold">{postData?.title}</h4>
+      <div className="overflow-hidden h-[23rem]">
+        <h4 className="text-xl font-semibold">{postData.title}</h4>
         <div className="flex gap-8 border-2 border-blue justify-start py-2">
           <h6>
             Posted On:{" "}
@@ -31,10 +30,14 @@ const SinglePost = () => {
                 "MMM dd, yyyy"
               )}
             </span>{" "}
-            by <span className="font-semibold">{postData?.author}</span>
+            by <span className="font-semibold">{postData.author}</span>
           </h6>
         </div>
-        <p className="text-justify pb-2">{postData?.content}</p>
+        <p className="text-justify pb-2">
+          {postData.content.split(" ").slice(0, 50).join(" ")}
+          {postData.content.split(" ").length > 50 ? "..." : ""}
+        </p>
+
         <img
           src={
             postData?.thumbnail
@@ -56,7 +59,7 @@ const SinglePost = () => {
           <FaRegShareSquare /> Share
         </button>
       </div>
-      <div className="">Comments</div>
+
       <CommentComponent></CommentComponent>
     </div>
   );
