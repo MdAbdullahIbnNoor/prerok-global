f1d943b74dfd2eb08c9f67b36b6340c97405a90d
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-02-25 08:48:55 +0600

    forum apis created

--- Prerok-Global-Server/app.js
+++ Prerok-Global-Server/app.js
@@ -1,37 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
-const userRoutes = require('./routes/user.routes');
-const addressRoutes = require('./routes/address.routes');
-const authRoutes = require('./routes/auth.routes');
-const paymentRoutes = require('./routes/payment.routes');
-const bookingRoutes = require('./routes/booking.routes');
-const trackingRoutes = require('./routes/tracking.routes')
-
-
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const addressRoutes = require("./routes/address.routes");
+const authRoutes = require("./routes/auth.routes");
+const paymentRoutes = require("./routes/payment.routes");
+const bookingRoutes = require("./routes/booking.routes");
+const trackingRoutes = require("./routes/tracking.routes");
+const forumRoutes = require("./routes/forum.routes");
 
 //configs
 require("./configs/database");
 
 //middlewares
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:5173", "https://prerokglobal.web.app"],
-  credentials: true
-}));
-app.use(cookieParser())
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://prerokglobal.web.app"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 //routes
-app.use('/api/users', userRoutes);
-app.use('/api/addressbook', addressRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/addressbook", addressRoutes);
 app.use(authRoutes);
-app.use('/api/payments', paymentRoutes);
-app.use('/api/bookings', bookingRoutes)
-app.use('/api/tracking', trackingRoutes);
-
-
+app.use("/api/payments", paymentRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/tracking", trackingRoutes);
+app.use("/api/forum", forumRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
---
+++ Prerok-Global-Server/models/forum.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const forumSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  comments: {
+    type: Array, 
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+});
+
+const Forum = mongoose.model("Forum", forumSchema);
+module.exports = Forum;
