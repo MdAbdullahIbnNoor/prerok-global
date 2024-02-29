acd7cf91356f64ed255822527b932a39f15a79c6
Abir <abirmahmud221@gmail.com>
2024-02-29 16:17:45 +0600

    added image bb api for dirrect photo uploads

--- Prerok-Global-Client/src/Layouts/Dashboard/User/Review/Review.jsx
+++ Prerok-Global-Client/src/Layouts/Dashboard/User/Review/Review.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
 import useAuth from "../../../../hooks/useAuth";
-
+import { FaUpload } from "react-icons/fa";
+import { imageUpload } from "../../../../api/imageUpload";
 
 const Review = () => {
+  const { user } = useAuth();
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [, setLoading] = useState(false);
 
-    const {user} = useAuth();
-   
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const review = form.review.value;
+    const image = selectedImage;
+    setLoading(true)
+            const { data: imageData } = await imageUpload(image);
     const data = {
-        name: user.displayName,
-        email: user.email,
-        image: user.photoURL
-    }
+      name: user.displayName,
+      email: user.email,
+      userImage: user.photoURL,
+      review,
+      reviewImage: imageData.display_url
+    };
     console.log(data);
-
+    setSelectedImage(null);
+    form.reset();
+    setLoading(false);
+  };
 
   return (
     <div>
@@ -25,64 +39,62 @@ const Review = () => {
           </div>
           {/* rating  */}
           <div className="flex justify-center mb-3">
-                <div className="rating">
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    
-                  />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    checked
-                  />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-              </div>
+            <div className="rating">
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+                checked
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+          </div>
           <hr />
           <div className="mt-2">
-            <form className="w-11/12 mx-auto">
-              
-              {/* title  */}
-              <input
-                type="text"
-                name="name"
-                className="border h-11 w-full px-4 py-2 mt-3 rounded-sm caret-[#f5ab35] focus:border-[#f5ab35]"
-                placeholder="Your Name"
-                id="name"
-              />{" "}
-              <br />
-              {/* Photo  */}
-              <input
-                type="text"
-                name="image"
-                className="border h-11 w-full px-4 py-2 mt-3 rounded-sm caret-[#f5ab35] focus:border-[#f5ab35]"
-                placeholder="IMAGE LINK"
-                id="image"
-              />{" "}
-              <br />
+            <form onSubmit={handleSubmit} className="w-11/12 mx-auto">
+              {/* image  */}
+              <div>
+                <label
+                  className="border block h-11 w-full px-4 py-2 mt-3 rounded-sm caret-[#f5ab35] focus:border-[#f5ab35] cursor-pointer"
+                  htmlFor="photoInput"
+                >
+                  <FaUpload className="inline mr-1 text-lg"></FaUpload>
+                  {selectedImage ? selectedImage.name : "Upload Photo(optional)"}
+                </label>
+                <input
+                  hidden
+                  onChange={(e) => setSelectedImage(e.target.files[0])}
+                  type="file"
+                  name=""
+                  id="photoInput"
+                />
+              </div>
               <textarea
                 type="text"
                 name="review"
                 className=" textarea textarea-bordered textarea-lg w-full  py-2 px-4 mt-3  rounded-sm caret-[#f5ab35]"
                 placeholder="Give Your Feedback ..."
                 id="review"
+                required
               />
               <button className="border h-11 w-full px-5 py-2 mt-3 bg-[#f5ab35] text-white font-bold rounded-sm hover:cursor-pointer transition-all duration-300 ease-in-out hover:bg-[#222222] flex items-center justify-center">
                 SUBMIT
