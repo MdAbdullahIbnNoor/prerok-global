7e2819e406d3460da016eccddb5dc9c7ef0e0d3e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-07 12:26:58 +0600

    make testimonial secion dynamic

--- Prerok-Global-Client/src/Components/Homepage/Testimonial/Testimonial.jsx
+++ Prerok-Global-Client/src/Components/Homepage/Testimonial/Testimonial.jsx
@@ -3,10 +3,24 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
+import { axiosSecure } from "../../../api/axiosInstances";
+import { useQuery } from "@tanstack/react-query";
 
 const Testimonial = () => {
+  const { data: testimonials = [], isLoading, refetch } = useQuery({
+    queryKey: ["testimonials"],
+    queryFn: async () => {
+      try {
+        const { data: allTestimonials } = await axiosSecure.get(`/api/testimonials/get-all-testimonials`);
+        return allTestimonials;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+      }
+    },
+  });
   return (
-    <div className="relative lg:h-[500px] max-w-full mb-12">
+    <div className="relative max-w-full py-6">
       {/* Static Background Image */}
       <div
         className="absolute inset-0 bg-cover bg-center"
@@ -28,78 +42,39 @@ const Testimonial = () => {
         }}
         modules={[Pagination]}
       >
-        <SwiperSlide>
-          <div className="p-4 m-4 bg-gray-600 bg-transparent rounded-lg overflow-hidden md:pt-4">
-            <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-              <section className="lg:py-14">
-                <div className="md:flex-col md:items-center md:justify-center">
-                  <div>
-                    <img
-                      alt="Taylor"
-                      loading="lazy"
-                      className="mx-auto h-20 w-3h-20 rounded-full border-4 border-amber-500 mb-2"
-                      src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    />
-                  </div>
-                  <div className="my-3 text-center md:mt-0 md:ml-4 md:flex-col md:items-center">
-                    <div className="text-base leading-6 font-medium text-gray-200">
-                      Taylor Otwell
+        {
+          testimonials?.map(testimonial => <SwiperSlide key={testimonial._id}>
+            <div className="p-4 m-4 bg-gray-600 bg-transparent rounded-lg overflow-hidden md:pt-4">
+              <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                <section className="lg:py-14">
+                  <div className="md:flex-col md:items-center md:justify-center">
+                    <div>
+                      <img
+                        alt="Taylor"
+                        loading="lazy"
+                        className="mx-auto h-20 w-20 object-cover rounded-full border-4 border-amber-500 mb-2 bg-white"
+                        src={testimonial?.userImage && testimonial?.userImage !== "null" ? testimonial?.userImage : "https://i.ibb.co/QCCPd4y/corporate-user-icon.png"}
+                      />
                     </div>
-                    <div className="text-base leading-6 font-medium text-gray-400">
-                      Creator of Laravel
+                    <div className="my-3 text-center md:mt-0 md:ml-4 md:flex-col md:items-center">
+                      <div className="text-base leading-6 font-medium text-gray-200">
+                        {testimonial?.name}
+                      </div>
+                      {/* <div className="text-base leading-6 font-medium text-gray-400">
+                        User
+                      </div> */}
+                    </div>
+                    <div className="max-w-3xl mx-auto text-center text-lg text-gray-100">
+                      <p>
+                        {testimonial?.review}
+                      </p>
                     </div>
                   </div>
-                  <div className="max-w-3xl mx-auto text-center text-lg text-gray-100">
-                    <p>
-                      Swift, reliable, and stress-free! My go-to for seamless
-                      deliveries. Exceptional service that exceeds expectations
-                      every time. Highly recommended!
-                    </p>
-                  </div>
-                </div>
-              </section>
+                </section>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="p-4 m-4 bg-gray-600 bg-transparent rounded-lg overflow-hidden md:pt-4">
-            <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-              <section className="lg:py-14">
-                <div className="md:flex-col md:items-center md:justify-center">
-                  <div>
-                    <img
-                      alt="Taylor"
-                      loading="lazy"
-                      className="mx-auto h-20 w-3h-20 rounded-full border-4 border-amber-500 mb-2"
-                      src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    />
-                  </div>
-                  <div className="my-3 text-center md:mt-0 md:ml-4 md:flex-col md:items-center">
-                    <div className="text-base leading-6 font-medium text-gray-200">
-                      John Doe
-                    </div>
-                    <div className="text-base leading-6 font-medium text-gray-400">
-                      CEO of Raj IT
-                    </div>
-                  </div>
-                  <div className="max-w-3xl mx-auto text-center text-lg text-gray-100">
-                    <p>
-                      Exceptional delivery service! Swift, reliable, and
-                      stress-free, they redefine efficiency. Seamless deliveries
-                      that exceed expectations every time. From prompt arrivals
-                      to secure handling, their service ensures peace of mind.
-                      Highly recommended for a superior and dependable shipping
-                      experience.
-                    </p>
-                  </div>
-                </div>
-              </section>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        {/* Add more SwiperSlide components for additional testimonials */}
+          </SwiperSlide>)
+        }
       </Swiper>
     </div>
   );
